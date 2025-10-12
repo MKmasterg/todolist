fdf54366a087a3ee3fc42abb9146756e37be5493
mk master <79350888+MKmasterg@users.noreply.github.com>
2025-10-12 20:22:35 +0330

    refactor: clean up in-memory database code

--- data/in_memory_db.py
+++ data/in_memory_db.py
@@ -11,15 +11,18 @@ from core.models import Project, Task
 projects_db: Dict[str, Project] = {}
 tasks_db: Dict[str, List[Task]] = {}  # key: project name, value: list of tasks
 
+
 def add_project(project: Project) -> None:
     projects_db[project.name] = project
     tasks_db[project.name] = []
+
 
 def get_project(name: str) -> Project:
     project: Project = projects_db.get(name, None)
     if project is None:
         raise ProjectNotFoundError(f"Project with name '{name}' not found.")
     return project
+
 
 def update_project_name(old_name: str, new_name: str) -> None:
     project: Project = projects_db.pop(old_name, None)
@@ -31,6 +34,7 @@ def update_project_name(old_name: str, new_name: str) -> None:
     projects_db[new_name] = project
     tasks_db[new_name] = tasks_db.pop(old_name)
 
+
 def delete_project(name: str) -> None:
     project: Project = projects_db.pop(name, None)
 
@@ -39,19 +43,21 @@ def delete_project(name: str) -> None:
 
     tasks_db.pop(name, None)
 
+
 def add_task(project_name: str, task: Task) -> None:
     if project_name not in tasks_db:
         raise ProjectNotFoundError(f"Project with name '{project_name}' not found.")
     tasks_db[project_name].append(task)
+
 
 def get_tasks(project_name: str) -> List[Task]:
     if project_name not in tasks_db:
         raise ProjectNotFoundError(f"Project with name '{project_name}' not found.")
     return tasks_db.get(project_name, [])
 
+
 def delete_task(project_name: str, task: Task) -> None:
     if project_name in tasks_db:
         tasks_db[project_name] = [t for t in tasks_db[project_name] if t != task]
     else:
         raise ProjectNotFoundError(f"Project with name '{project_name}' not found.")
-
